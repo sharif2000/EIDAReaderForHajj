075f6d5a26e3fde781ab28b24b894f096be88a30
Sherif Abdellatif <sharif_pc@hotmail.com>
2017-12-24 11:00:46 +0400

    Created Solution + Added blank Application Page

---
+++ EIDAReaderForHajj/Layouts/EIDAReaderForHajj/NewHajjFormWithEIDAReader.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace EIDAReaderForHajj.Layouts.EIDAReaderForHajj
+{
+    public partial class NewHajjFormWithEIDAReader : LayoutsPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
